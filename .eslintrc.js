f947b6c4b5b762534c11fd4fea04843f377174e8
ibfpig <thlz998@gmail.com>
2019-04-04 20:29:22 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    root: true,
+    parser: 'babel-eslint',
+    parserOptions: {
+        'ecmaVersion': 6,
+        'sourceType': 'module'
+    },
+    env: {
+        node: true,
+    },
+    extends: ['google'],
+    rules: {}
+}
